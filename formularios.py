743da08b025b2eef56adced5f911f91c7813c9c6
Juan Manuel Rassa <jmrassaproduccion@gmail.com>
2021-10-12 06:35:54 -0500

    Ajustando arquitectura de directorios y carpetas.

--- formularios.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, TextField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired
-
-class Login(FlaskForm):
-    userId = TextField('Usuario *', validators = [InputRequired(message = 'El usuario es requerido')])
-    clave = PasswordField('Contraseña *', validators = [InputRequired(message = 'La clave es requerida')])
-    enviar = SubmitField('Entrar')   
