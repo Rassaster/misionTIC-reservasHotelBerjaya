8eaed18c2a5ef293157cf661bfb32d01652402c2
andresmunevar@github.com <afmunene@gmail.com>
2021-10-23 12:14:27 -0300

    modificando Fields en registro

--- src/formularios.py
+++ src/formularios.py
@@ -1,21 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, TextField, DateField
+from wtforms import PasswordField, SubmitField, TextField, DateField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, EqualTo
 
 class Login(FlaskForm):
-	email = EmailField('E-mail *', validators = [InputRequired(message = 'El email es requerido')])
-	clave = PasswordField('Contraseña *', validators = [InputRequired(message = 'La clave es requerida')])
+	email = EmailField('E-mail *')
+	# validators = [InputRequired(message = 'El email es requerido')])
+	clave = PasswordField('Contraseña *')
+	# validators = [InputRequired(message = 'La clave es requerida')])
 	logIn = SubmitField('Ingresar')
 	signUp = SubmitField('Registarse')
 
 class Registro(FlaskForm):    
 	nombre = TextField('Nombre ')
 	apellido = TextField('Apellido ')
+	tipoDoc = SelectField(u'Tipo de documento', choices=[('Cedula'), ('Pasaporte'), ('Cedula de extranjeria')])
 	documento = TextField('Documento ')
 	fechaIn = DateField('Fecha de Entrada ')
-	logIn = SubmitField('Ingresar')
-	signUp = SubmitField('Registrarse')
+	fechaOut = DateField('Fecha de Salida ')
+	tipoHab = SelectField(u'Tipo de Habitacion', choices=[('Familiar'), ('Deluxe'), ('Emperatriz')])
+	numeroHab = TextField('Numero Habitacion ')
+	precioHab = TextField('Precio Habitacion ')
+	reserva = SubmitField('Ingresar')
 
 class NuevoUsr(FlaskForm):    
 	email = EmailField('E-Mail *', validators = [InputRequired(message='El email es requerido')])
