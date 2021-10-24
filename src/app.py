14b9ecd0efda4cbce54b4a1d56a2cadeef7241bb
Reinaldo Omar <reymar.mendoza@gmail.com>
2021-10-24 14:28:59 -0500

    funciona el enviar desde el form a la bd, pero aun esta incompleto, sin embargo esta validando el tipo de danto antes de guardar

--- src/app.py
+++ src/app.py
@@ -26,22 +26,26 @@ def login():
         ema = escape(frm.email.data.strip())
         cla = escape(frm.clave.data.strip())
 
-        sql = f'SELECT usuario, contrasena FROM fullTable WHERE usuario="{ema}"'
-        res = seleccion(sql)
+        try:
+            sql = f'SELECT usuario, contrasena FROM fullTable WHERE usuario="{ema}"'
+            res = seleccion(sql)
 
-        if len(res) == 0:
-            flash('ERROR: Email o clave invalidas')
-            return redirect('/login/')
-        else:
-            claveHash = res[0][1]
-            if check_password_hash(claveHash, cla):
-                session.clear()
-                session['usuario'] = res[0][0]
-                session['contrasena'] = res[0][1]
-                return redirect('/registro/')
-            else:
+            if len(res) == 0:
                 flash('ERROR: Email o clave invalidas')
                 return redirect('/login/')
+            else:
+                claveHash = res[0][1]
+                if check_password_hash(claveHash, cla):
+                    session.clear()
+                    session['usuario'] = res[0][0]
+                    session['contrasena'] = res[0][1]
+                    return redirect('/registro/')
+                else:
+                    flash('ERROR: Email o clave invalidas')
+                    return redirect('/login/')
+
+        except Exception as ex:
+            print(ex)
 
     return render_template('login.html', form=frm, titulo='Login')
 
@@ -56,31 +60,36 @@ def nuevoUsr():
         clave1 = escape(request.form['passn'])
         clave2 = escape(request.form['passv'])
 
-        sql = f"SELECT COUNT(usuario) FROM fullTable WHERE usuario='{email}'"
-        res = seleccion(sql)
+        try:
+            sql = f"SELECT COUNT(usuario) FROM fullTable WHERE usuario='{email}'"
+            res = seleccion(sql)
 
-        if res[0][0] > 0:
-            flash('ERROR: Por favor use otro correo, el ingresado ya existe')
-        elif email == None or len(email) == 0:
-            flash('ERROR: Debe suministrar un email válido')
-        elif clave1 == None or len(clave1) == 0 or not pass_valido(clave1):
-            flash('ERROR: Debe suministrar una clave válida')
-        elif clave2 == None or len(clave2) == 0 or not pass_valido(clave2):
-            flash('ERROR: Debe suministrar una verificación de clave válida')
-        elif clave1 != clave2:
-            flash('ERROR: La clave y la verificación no coinciden')
-        else:
-            sql = f"INSERT INTO fullTable (usuario, contrasena) VALUES (?, ?)"
-            pwd = generate_password_hash(clave1)
-            res = accion(sql,(email, pwd))
-            if res != 0:
-                flash('INFO: Datos almacenados con exito')
-                session.clear()
-                session['usuario'] = email
-                session['contrasena'] = pwd
-                return redirect('/registro/')
+            if res[0][0] > 0:
+                flash('ERROR: Por favor use otro correo, el ingresado ya existe')
+            elif email == None or len(email) == 0:
+                flash('ERROR: Debe suministrar un email válido')
+            elif clave1 == None or len(clave1) == 0 or not pass_valido(clave1):
+                flash('ERROR: Debe suministrar una clave válida')
+            elif clave2 == None or len(clave2) == 0 or not pass_valido(clave2):
+                flash('ERROR: Debe suministrar una verificación de clave válida')
+            elif clave1 != clave2:
+                flash('ERROR: La clave y la verificación no coinciden')
             else:
-                flash('ERROR: Por favor reintente')
+                sql = f"INSERT INTO fullTable (usuario, contrasena) VALUES (?, ?)"
+                pwd = generate_password_hash(clave1)
+                res = accion(sql,(email, pwd))
+                print(f'res: {res}')
+
+                if res != 0:
+                    flash('INFO: Datos almacenados con exito')
+                    session.clear()
+                    session['usuario'] = email
+                    session['contrasena'] = pwd
+                    return redirect('/registro/')
+                else:
+                    flash('ERROR: Por favor reintente')
+        except Exception as ex:
+            print(ex)
 
         return render_template('nuevoUsr.html', form=frm, titulo='Nuevo usuario')
 
@@ -91,19 +100,55 @@ def registro():
 
     if request.method == 'GET':
         print(f'INFO: Sesion iniciada para: {usr}')
-        return render_template('registro.html',form=frm, titulo='Registro')
-    elif frm.reserva.data:
-        nom = escape(request.form['nombre'])
-        ape = escape(request.form['apellido'])
-        tipoDoc = escape(request.form['tipoDoc'])
-        doc = escape(request.form['documento'])
+        return render_template('registro.html', form=frm, titulo='Registro')
+    else:
+        nom = str(escape(request.form['nombre']))
+        ape = str(escape(request.form['apellido']))
+        tipoDoc = str(escape(request.form['tipoDoc']))
+        doc = int(escape(request.form['documento']))
         dateIn = escape(request.form['fechaIn'])
         dateOut = escape(request.form['fechaOut'])
         tipoHab = escape(request.form['tipoHab'])
         numHab = escape(request.form['numeroHab'])
-        
-        flash('Realizar consulta con: '+ nom + ' / ' + ape + ' / ' + tipoDoc + ' / ' + doc + ' / ' + dateIn + ' / ' + dateOut + ' / ' + tipoHab + ' / ' + numHab)
-        print(f"Realizar consulta con: '+ {{ nom }} + ' / ' + {{ ape }} + ' / ' + {{ tipoDoc }} + ' / ' + {{ doc }} + ' / ' + {{ dateIn }} + ' / ' + {{ dateOut }} + ' / ' + {{ tipoHab }} + ' / ' + {{ numHab }}")
+
+        try:
+            # sql = f"INSERT INTO fullTable (nombre, apellido) VALUES ('{nom}', '{ape}')"
+            # type(usuario) is str
+            # type(contrasena) is str
+            # type(num_hab) is int
+            # type(caract) is str
+            # type(precio) is int
+            # type(estado) is str
+            # type(fecha_in) is str
+            # type(fecha_sal) is str
+            # type(calific) is int
+            # type(comen) is str
+            # type(pago) is bool
+            # type(rol) is str
+
+            if isinstance(nom, str) and isinstance(ape, str) and isinstance(tipoDoc, str) and isinstance(doc, int) : 
+
+                sql = f"INSERT INTO fullTable (nombre, apellido, tipo_doc, num_doc) VALUES (?, ?, ?, ?)"
+                # res = accion(sql,(nom, ape, tipoDoc, int(doc), tipoHab, int(numHab)))
+                res = accion(sql, (nom, ape, tipoDoc, doc))
+                
+                if res != 0:
+                    print('INFO: Datos almacenados con exito')
+                    flash('INFO: Datos almacenados con exito')
+                    # return redirect('/registro/')
+                    return redirect('/gracias/')
+                else:
+                    print('ELSE error')
+                    flash('ERROR: Por favor reintente')
+
+            else:
+                print('TYPES NOT MATCH')
+
+        except Exception as ex:
+            print(f'ex100: {ex}')
+
+        flash(f"Realizar consulta con: { nom }, { ape }, { tipoDoc }, { doc }, { dateIn }, { dateOut }, { tipoHab }, { numHab },")
+        print(f"Realizar consulta con: { nom }, { ape }, { tipoDoc }, { doc }, { dateIn }, { dateOut }, { tipoHab }, { numHab },")
 
         # for msg in get_flashed_messages():
         #     print(f'flashed_messages: {msg}')
@@ -141,27 +186,28 @@ def instalaciones():
 def comentarios():
     habitacion = escape(request.args.get('habitacion', 'error'))
 
-    sql = f"SELECT nombre, apellido, comen, calific FROM fullTable WHERE caract='{habitacion}'"
-    res = seleccion(sql)
-    if len(res) == 0:
-        dat = None
-        msg = 'No existen registros'
-    else:
-        dat = res
-        msg ='Se muestran los datos'
-    
-    contexto = {
-        'data' : dat, 
-        'qRes' : msg, 
-        'titulo' : 'Comentarios'
-    }
+    try:
+        sql = f"SELECT nombre, apellido, comen, calific FROM fullTable WHERE caract='{habitacion}'"
+        res = seleccion(sql)
+        if len(res) == 0:
+            dat = None
+            msg = 'No existen registros'
+        else:
+            dat = res
+            msg ='Se muestran los datos'
+        
+        contexto = {
+            'data' : dat, 
+            'qRes' : msg, 
+            'titulo' : 'Comentarios'
+        }
+    except Exception as ex:
+        print(ex)
 
     return render_template('comentarios.html', **contexto)
 
 @app.route('/gracias/')
 def gracias():
-    if request.method == 'GET':
-        print('gerrrr')
     return render_template('gracias.html', titulo='Gracias')
 
 if __name__ == '__main__':
